2d9118f4058432a66d1101671b8184bdec5852a0
randomtestfive <randomtestfive@gmail.com>
2018-01-08 19:47:02 -0500

    move stuff

--- src/org/redalert1741/robotBase/config/Configurable.java
+++
@@ -1,6 +0,0 @@
-package org.redalert1741.robotBase.config;
-
-public interface Configurable
-{
-	public abstract void reloadConfig();
-}
--- src/org/redalert1741/robotBase/logging/Loggable.java
+++
@@ -1,7 +0,0 @@
-package org.redalert1741.robotBase.logging;
-
-public interface Loggable
-{
-	public abstract void setupLogging(DataLogger logger);
-	public abstract void log(DataLogger logger);
-}
