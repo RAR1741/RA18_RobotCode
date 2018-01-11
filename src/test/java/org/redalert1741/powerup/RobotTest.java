dc6977bff541a9d132d31f61530fdf8ef40001ed
Jordan Miller <jmiller@springbuk.com>
2018-01-10 21:02:26 -0500

    wip

---
+++ src/test/java/org/redalert1741/powerup/RobotTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.redalert1741.powerup.Robot;
+
+class RobotTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void mySecondTest() {
+        assertEquals(3, 1 + 1);
+    }
+
+    @Test
+    void myThirdTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
