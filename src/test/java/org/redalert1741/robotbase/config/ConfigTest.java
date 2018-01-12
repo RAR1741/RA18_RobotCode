5b52991142d9671811c8586ee6c55ae09fd34f72
RandomTestFive <randomtestfive@gmail.com>
2018-01-12 14:52:41 -0500

    add config tests

--- src/test/java/org/redalert1741/robotbase/config/ConfigTest.java
+++ src/test/java/org/redalert1741/robotbase/config/ConfigTest.java
@@ -14,4 +14,24 @@ public class ConfigTest
 		boolean success = Config.loadFromFile(getClass().getResource("test_config.txt").getPath());
 		assertTrue(success, "Could not open test_config.txt");
     }
+	
+	@Test
+    void configTypeTest() {
+		Config.loadFromFile(getClass().getResource("test_config.txt").getPath());
+		assertEquals("string", Config.getSetting("string_value", "wrong string"));
+		assertEquals(5.5, Config.getSetting("double_value", 1.0));
+		assertEquals(true, Config.getSetting("boolean_value", false));
+    }
+	
+	@Test
+    void configCommentTest() {
+		Config.loadFromFile(getClass().getResource("test_config.txt").getPath());
+		assertEquals(20, Config.getSetting("comment_value", 30));
+    }
+	
+	@Test
+    void configDefaultValueTest() {
+		Config.loadFromFile(getClass().getResource("test_config.txt").getPath());
+		assertEquals("default value", Config.getSetting("nonexistant_value", "default value"));
+    }
 }
