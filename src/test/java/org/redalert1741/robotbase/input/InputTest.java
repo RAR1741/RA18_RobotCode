ae9ed3ec62f61ca80b9db6b9f6b556713bf05c6e
Jordan Miller <jmiller@springbuk.com>
2018-01-13 11:40:15 -0500

    Just a little formatting clean up

--- src/test/java/org/redalert1741/robotbase/input/InputTest.java
+++ src/test/java/org/redalert1741/robotbase/input/InputTest.java
@@ -7,9 +7,9 @@ import org.redalert1741.robotBase.input.EdgeDetect;
 
 class InputTest {
 
-	/**
-	 * Tests {@link EdgeDetect} with no changes to state
-	 */
+  	/**
+  	 * Tests {@link EdgeDetect} with no changes to state
+  	 */
     @Test
     void edgeDetectNoChangeTest() {
     	EdgeDetect edge = new EdgeDetect();
@@ -17,12 +17,13 @@ class InputTest {
         	assertEquals(false, edge.Check(false));
         }
     }
+
     /**
      * Tests {@link EdgeDetect} with one change to state
      */
     @Test
     void edgeDetectOneChangeTest() {
-    	EdgeDetect edge = new EdgeDetect();
+        EdgeDetect edge = new EdgeDetect();
         for(int i = 0; i < 5; i++) {
         	assertEquals(false, edge.Check(false));
         }
@@ -31,13 +32,13 @@ class InputTest {
         	assertEquals(false, edge.Check(true));
         }
     }
-    
+
     /**
      * Tests {@link EdgeDetect} by alternating states, starting with false.
      */
     @Test
     void edgeDetectAlternateTest() {
-    	EdgeDetect edge = new EdgeDetect();
+        EdgeDetect edge = new EdgeDetect();
         for(int i = 0; i < 10; i++) {
         	assertEquals(i%2!=0, edge.Check(i%2!=0));
         }
