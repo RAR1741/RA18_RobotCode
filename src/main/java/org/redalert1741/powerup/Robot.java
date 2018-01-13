3fa38d822c2881b8bd56fdf8919cc9559e6e10fc
randomtestfive <randomtestfive@gmail.com>
2018-01-13 15:05:47 -0500

    add pneumatics

--- src/main/java/org/redalert1741/powerup/Robot.java
+++ src/main/java/org/redalert1741/powerup/Robot.java
@@ -2,20 +2,26 @@ package org.redalert1741.powerup;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Robot extends IterativeRobot
 {
 	TankDrive tank;
 	XboxController driver;
+	DoubleSolenoid grabber;
 
 	@Override
 	public void robotInit() {
 	    //controllers
 	    driver = new XboxController(0);
-	    
+
 	    //drivetrain
 	    tank = new TankDrive(1,2,3,4,5,6);
+
+	    //grabber
+	    grabber = new DoubleSolenoid(0, 1);
 	}
 
 	@Override
@@ -41,6 +47,12 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopPeriodic() {
 	    tank.arcadeDrive(driver.getX(Hand.kRight) * -0.6, driver.getY(Hand.kLeft) * 0.6);
+
+	    if(driver.getAButton()) {
+		grabber.set(Value.kForward);
+	    } else if (driver.getBButton()) {
+		grabber.set(Value.kReverse);
+	    }
 	}
 
 	@Override
