c1525e48e99f0d913959beea758f0a6079c648c9
randomtestfive <randomtestfive@gmail.com>
2018-01-13 15:18:13 -0500

    more space

--- src/main/java/org/redalert1741/powerup/Robot.java
+++ src/main/java/org/redalert1741/powerup/Robot.java
@@ -14,14 +14,14 @@ public class Robot extends IterativeRobot
 
     @Override
     public void robotInit() {
-	//controllers
-	driver = new XboxController(0);
+        //controllers
+        driver = new XboxController(0);
 
-	//drivetrain
-	tank = new TankDrive(1,2,3,4,5,6);
+        //drivetrain
+        tank = new TankDrive(1,2,3,4,5,6);
 
-	//grabber
-	grabber = new DoubleSolenoid(0, 1);
+        //grabber
+        grabber = new DoubleSolenoid(0, 1);
     }
 
     @Override
@@ -32,13 +32,13 @@ public class Robot extends IterativeRobot
 
     @Override
     public void teleopPeriodic() {
-	tank.arcadeDrive(driver.getX(Hand.kRight) * -0.6, driver.getY(Hand.kLeft) * 0.6);
+        tank.arcadeDrive(driver.getX(Hand.kRight) * -0.6, driver.getY(Hand.kLeft) * 0.6);
 
-	if(driver.getAButton()) {
-	    grabber.set(Value.kForward);
-	} else if (driver.getBButton()) {
-	    grabber.set(Value.kReverse);
-	}
+        if(driver.getAButton()) {
+            grabber.set(Value.kForward);
+        } else if (driver.getBButton()) {
+            grabber.set(Value.kReverse);
+        }
     }
 
     @Override
--- src/main/java/org/redalert1741/powerup/TankDrive.java
+++ src/main/java/org/redalert1741/powerup/TankDrive.java
@@ -4,33 +4,33 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class TankDrive {
-	TalonSRX left1, left2, left3, right1, right2, right3;
+    TalonSRX left1, left2, left3, right1, right2, right3;
+
+    public TankDrive(int l1, int l2, int l3, int r1, int r2, int r3) {
+        left1 = new TalonSRX(l1);
+        left2 = new TalonSRX(l2);
+        left3 = new TalonSRX(l3);
+
+        right1 = new TalonSRX(r1);
+        right2 = new TalonSRX(r2);
+        right3 = new TalonSRX(r3);
+
+        left1.setInverted(true);
+        left2.setInverted(true);
+        left3.setInverted(true);
+    }
 	
-	public TankDrive(int l1, int l2, int l3, int r1, int r2, int r3) {
-		left1 = new TalonSRX(l1);
-		left2 = new TalonSRX(l2);
-		left3 = new TalonSRX(l3);
-		
-		right1 = new TalonSRX(r1);
-		right2 = new TalonSRX(r2);
-		right3 = new TalonSRX(r3);
-		
-		left1.setInverted(true);
-		left2.setInverted(true);
-		left3.setInverted(true);
-	}
-	
-	public void tankDrive(double left, double right) {
-		left1.set(ControlMode.PercentOutput, left);
-		left2.set(ControlMode.PercentOutput, left);
-		left3.set(ControlMode.PercentOutput, left);
-		
-		right1.set(ControlMode.PercentOutput, right);
-		right2.set(ControlMode.PercentOutput, right);
-		right3.set(ControlMode.PercentOutput, right);
-	}
-	
-	public void arcadeDrive(double x, double y) {
-		tankDrive(y+x, y-x);
-	}
+    public void tankDrive(double left, double right) {
+	    left1.set(ControlMode.PercentOutput, left);
+	    left2.set(ControlMode.PercentOutput, left);
+	    left3.set(ControlMode.PercentOutput, left);
+
+	    right1.set(ControlMode.PercentOutput, right);
+	    right2.set(ControlMode.PercentOutput, right);
+	    right3.set(ControlMode.PercentOutput, right);
+    }
+
+    public void arcadeDrive(double x, double y) {
+        tankDrive(y+x, y-x);
+    }
 }
