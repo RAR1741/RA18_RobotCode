bec6daa3a831c5b7254e06c97c2b8ad25bbdb000
Jordan Miller <jmiller@springbuk.com>
2018-01-13 14:25:38 -0500

    Updated indentation and stuffn

--- src/test/java/org/redalert1741/robotbase/input/InputTest.java
+++ src/test/java/org/redalert1741/robotbase/input/InputTest.java
@@ -6,15 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.redalert1741.robotBase.input.EdgeDetect;
 
 class InputTest {
-
-  	/**
-  	 * Tests {@link EdgeDetect} with no changes to state
-  	 */
+    /**
+     * Tests {@link EdgeDetect} with no changes to state
+     */
     @Test
     void edgeDetectNoChangeTest() {
-    	EdgeDetect edge = new EdgeDetect();
+        EdgeDetect edge = new EdgeDetect();
         for(int i = 0; i < 10; i++) {
-        	assertEquals(false, edge.Check(false));
+            assertEquals(false, edge.Check(false));
         }
     }
 
@@ -25,11 +24,11 @@ class InputTest {
     void edgeDetectOneChangeTest() {
         EdgeDetect edge = new EdgeDetect();
         for(int i = 0; i < 5; i++) {
-        	assertEquals(false, edge.Check(false));
+            assertEquals(false, edge.Check(false));
         }
         assertEquals(true, edge.Check(true));
         for(int i = 0; i < 5; i++) {
-        	assertEquals(false, edge.Check(true));
+            assertEquals(false, edge.Check(true));
         }
     }
 
@@ -40,8 +39,7 @@ class InputTest {
     void edgeDetectAlternateTest() {
         EdgeDetect edge = new EdgeDetect();
         for(int i = 0; i < 10; i++) {
-        	assertEquals(i%2!=0, edge.Check(i%2!=0));
+            assertEquals(i%2!=0, edge.Check(i%2!=0));
         }
     }
-
 }
