7da53c03ae5a565991b60330ad7569e9e35dc712
randomtestfive <randomtestfive@gmail.com>
2018-01-17 19:55:49 -0500

    add a newline

--- src/main/java/org/redalert1741/robotbase/logging/Loggable.java
+++ src/main/java/org/redalert1741/robotbase/logging/Loggable.java
@@ -2,5 +2,6 @@ package org.redalert1741.robotbase.logging;
 
 public interface Loggable {
     public abstract void setupLogging(DataLogger logger);
+
     public abstract void log(DataLogger logger);
 }
