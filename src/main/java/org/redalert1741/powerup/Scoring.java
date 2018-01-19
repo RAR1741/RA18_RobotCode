798a7ed53e6a5cbb5c4103f66fbfbbe44c0d0ac1
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2018-01-19 18:58:20 -0500

    add javadoc to Scoring plus setIntake

--- src/main/java/org/redalert1741/powerup/Scoring.java
+++ src/main/java/org/redalert1741/powerup/Scoring.java
@@ -8,9 +8,19 @@ import org.redalert1741.robotbase.logging.DataLogger;
 import org.redalert1741.robotbase.logging.Loggable;
 
 public class Scoring implements Loggable {
-    Spark left, right;
+    Spark left;
+    Spark right;
     DoubleSolenoid grabber;
     
+    /**
+     * Constructor for scoring subsystem.
+     * @param leftP PWM channel of left spark
+     * @param rightP PWM channel of right spark
+     * @param grab ID 1 for DoubleSolenoid
+     * @param drop ID 2 for DoubleSolenoid
+     * @see Spark
+     * @see DoubleSolenoid
+     */
     public Scoring(int leftP, int rightP, int grab, int drop) {
         this.left = new Spark(leftP);
         this.right = new Spark(rightP);
@@ -23,6 +33,11 @@ public class Scoring implements Loggable {
 
     public void close() {
         grabber.set(Value.kReverse);
+    }
+    
+    public void setIntake(double input) {
+        left.set(input);
+        right.set(-input);
     }
 
     @Override
