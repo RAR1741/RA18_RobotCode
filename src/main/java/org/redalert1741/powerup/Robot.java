19eb2c80374a04d227023eefa789828cae8454e8
Patrick Frampton <pframpton@salesforce.com>
2018-01-19 20:55:26 -0500

    Add logger

--- src/main/java/org/redalert1741/powerup/Robot.java
+++ src/main/java/org/redalert1741/powerup/Robot.java
@@ -1,17 +1,30 @@
 package org.redalert1741.powerup;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Robot extends IterativeRobot
 {
+    private static final Logger logger = LogManager.getLogger(); // allocates logger for this class
     @Override
-    public void robotInit() {}
+    public void robotInit() {
+      logger.info("Robot startup started");
+
+      logger.info("Robot startup complete");
+    }
 
     @Override
-    public void autonomousInit() {}
+    public void autonomousInit() {
+      logger.info("Autonomous init started");
+
+      logger.info("Autonomous init complete");
+    }
 
     @Override
-    public void autonomousPeriodic() {}
+    public void autonomousPeriodic() {
+
+    }
 
     @Override
     public void teleopPeriodic() {}
