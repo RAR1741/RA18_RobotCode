8e456fe2393de45b611d509addcfb919f1fb3557
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2018-01-20 14:36:05 -0500

    add whitespace

--- src/main/java/org/redalert1741/powerup/Manipulation.java
+++ src/main/java/org/redalert1741/powerup/Manipulation.java
@@ -3,7 +3,6 @@ package org.redalert1741.powerup;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Spark;
-
 import org.redalert1741.robotbase.logging.DataLogger;
 import org.redalert1741.robotbase.logging.Loggable;
 
