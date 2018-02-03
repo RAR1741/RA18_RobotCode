6842972814422a81ecaab165dedbef297e179a82
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2018-02-03 10:27:40 -0500

    add solenoid for brake

--- src/main/java/org/redalert1741/powerup/Manipulation.java
+++ src/main/java/org/redalert1741/powerup/Manipulation.java
@@ -5,22 +5,27 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Spark;
 import org.redalert1741.robotbase.logging.DataLogger;
 import org.redalert1741.robotbase.logging.Loggable;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Manipulation implements Loggable {
     DoubleSolenoid tilt;
     Spark lift;
+    Solenoid brake;
     
     /**
      * Constructor for manipulation subsystem.
      * @param liftId PWM channel of lift spark
      * @param in ID 1 for DoubleSolenoid
      * @param out ID 2 for DoubleSolenoid
+     * @param b1 PCM ID of brake
      * @see Spark
      * @see DoubleSolenoid
+     * @see Solenoid
      */
-    public Manipulation(int liftId, int in, int out) {
+    public Manipulation(int liftId, int in, int out, int b1) {
         this.lift = new Spark(liftId);
         this.tilt = new DoubleSolenoid(in, out);
+        this.brake = new Solenoid(b1);
     }
     
     public void tiltIn() {
@@ -35,13 +40,23 @@ public class Manipulation implements Loggable {
         lift.set(input);
     }
     
+    public void stop() {
+        brake.set(true);
+    }
+    
+    public void go() {
+        brake.set(false);
+    }
+    
     @Override
     public void setupLogging(DataLogger logger) {
         logger.addAttribute("tilt_state");
+        logger.addAttribute("brake_state");
     }
 
     @Override
     public void log(DataLogger logger) {
         logger.log("tilt_state", tilt.get());
+        logger.log("brake_state", brake.get());
     }
 }
