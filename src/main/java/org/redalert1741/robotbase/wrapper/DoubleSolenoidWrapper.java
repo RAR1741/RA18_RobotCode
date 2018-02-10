52b42b066e2547ffa8e8baab73f2f6139937f66d
randomtestfive <randomtestfive@gmail.com>
2018-02-10 12:52:36 -0500

    wow doublesolenoid logging

--- src/main/java/org/redalert1741/robotbase/wrapper/DoubleSolenoidWrapper.java
+++ src/main/java/org/redalert1741/robotbase/wrapper/DoubleSolenoidWrapper.java
@@ -2,8 +2,23 @@ package org.redalert1741.robotbase.wrapper;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
-public interface DoubleSolenoidWrapper {
-    public void set(Value value);
+import org.redalert1741.robotbase.logging.DataLogger;
+import org.redalert1741.robotbase.logging.Loggable;
 
-    public Value get();
+public abstract class DoubleSolenoidWrapper implements Loggable {
+    protected String logname;
+
+    public abstract void set(Value value);
+
+    public abstract Value get();
+
+    @Override
+    public void setupLogging(DataLogger logger) {
+        logger.addAttribute(logname+"_state");
+    }
+
+    @Override
+    public void log(DataLogger logger) {
+        logger.log(logname+"_state", get());
+    }
 }
--- src/main/java/org/redalert1741/robotbase/wrapper/RealDoubleSolenoidWrapper.java
+++ src/main/java/org/redalert1741/robotbase/wrapper/RealDoubleSolenoidWrapper.java
@@ -3,7 +3,7 @@ package org.redalert1741.robotbase.wrapper;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
-public class RealDoubleSolenoidWrapper implements DoubleSolenoidWrapper {
+public class RealDoubleSolenoidWrapper extends DoubleSolenoidWrapper {
     private DoubleSolenoid doublesolenoid;
     
     public RealDoubleSolenoidWrapper(int id1, int id2) {
