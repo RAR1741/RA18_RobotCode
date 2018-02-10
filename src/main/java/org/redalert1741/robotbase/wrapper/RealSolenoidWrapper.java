d07cbc4800a1561685d2322168f8f45fc6de423e
randomtestfive <randomtestfive@gmail.com>
2018-02-10 12:34:32 -0500

    init logname

--- src/main/java/org/redalert1741/robotbase/wrapper/RealSolenoidWrapper.java
+++ src/main/java/org/redalert1741/robotbase/wrapper/RealSolenoidWrapper.java
@@ -5,8 +5,23 @@ import edu.wpi.first.wpilibj.Solenoid;
 public class RealSolenoidWrapper extends SolenoidWrapper {
     private Solenoid solenoid;
 
+    /**
+     * A real solenoid. Logging name is "solenoid_{id}"
+     * @param id PCM ID
+     */
     public RealSolenoidWrapper(int id) {
         solenoid = new Solenoid(id);
+        logname = "solenoid_"+id;
+    }
+
+    /**
+     * A real solenoid.
+     * @param id PCM ID
+     * @param name logging name
+     */
+    public RealSolenoidWrapper(int id, String name) {
+        solenoid = new Solenoid(id);
+        logname = name;
     }
 
     @Override
