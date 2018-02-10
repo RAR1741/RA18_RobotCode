c43b6af702dafdf37c8e3be7adee6f37bae50545
randomtestfive <randomtestfive@gmail.com>
2018-02-09 20:35:18 -0500

    wowzo more pid

--- src/main/java/org/redalert1741/robotbase/wrapper/RealTalonSrxWrapper.java
+++ src/main/java/org/redalert1741/robotbase/wrapper/RealTalonSrxWrapper.java
@@ -1,15 +1,28 @@
 package org.redalert1741.robotbase.wrapper;
 
+import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class RealTalonSrxWrapper implements TalonSrxWrapper {
+import org.redalert1741.robotbase.config.Config;
+import org.redalert1741.robotbase.logging.DataLogger;
+
+public class RealTalonSrxWrapper extends TalonSrxWrapper {
     private TalonSRX talon;
     private double value;
+    private String logname;
+    private int timeout;
 
     public RealTalonSrxWrapper(int id) {
         talon = new TalonSRX(id);
         value = 0;
+        logname = "talon_" + id;
+    }
+
+    public RealTalonSrxWrapper(int id, String name) {
+        talon = new TalonSRX(id);
+        value = 0;
+        logname = name;
     }
     
     @Override
@@ -45,4 +58,62 @@ public class RealTalonSrxWrapper implements TalonSrxWrapper {
         return value;
     }
 
+    @Override
+    public ErrorCode setP(double pValue) {
+        return talon.config_kP(0, pValue, timeout);
+    }
+
+    @Override
+    public ErrorCode setI(double iValue) {
+        return talon.config_kI(0, iValue, timeout);
+    }
+
+    @Override
+    public ErrorCode setD(double dValue) {
+        return talon.config_kD(0, dValue, timeout);
+    }
+
+    @Override
+    public ErrorCode setF(double kValue) {
+        return talon.config_kF(0, kValue, timeout);
+    }
+
+    @Override
+    public ErrorCode configNominalOutputForward(double value) {
+        return talon.configNominalOutputForward(value, timeout);
+    }
+
+    @Override
+    public ErrorCode configNominalOutputReverse(double value) {
+        return talon.configNominalOutputReverse(value, timeout);
+    }
+
+    @Override
+    public ErrorCode configPeakOutputForward(double value) {
+        return talon.configPeakOutputForward(value, timeout);
+    }
+
+    @Override
+    public ErrorCode configPeakOutputReverse(double value) {
+        return talon.configPeakOutputReverse(value, timeout);
+    }
+
+    @Override
+    public void setupLogging(DataLogger logger) {
+        logger.addAttribute(logname+"_current");
+        logger.addAttribute(logname+"_voltage");
+        logger.addAttribute(logname+"_value");
+    }
+
+    @Override
+    public void log(DataLogger logger) {
+        logger.log(logname+"_current", getOutputCurrent());
+        logger.log(logname+"_voltage", getBusVoltage());
+        logger.log(logname+"_value", get());
+    }
+
+    @Override
+    public void reloadConfig(Config config) {
+        timeout = config.getSetting("can_timeout", 10.0).intValue();
+    }
 }
